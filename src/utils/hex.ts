19e98a045cabff320e99d90e478d566a1392e5b2
Viki Val <viktorko99@gmail.com>
2022-01-16 11:37:05 +0100

    :zap: can convert address to Hex

--- src/utils/hex.ts
+++ src/utils/hex.ts
@@ -1,5 +1,14 @@
-import { hexToString , isHex } from '@polkadot/util'
+import { hexToString , isHex, u8aToHex } from '@polkadot/util'
+import { decodeAddress } from '@polkadot/keyring'
 
 export const decodeHex = (hex: string): string => {
   return isHex(hex) ? hexToString(hex) : hex
+}
+
+export const addressToHex = (address: string): string => {
+  return isHex(address) ? address : accountToPublicKey(address)
+}
+
+const accountToPublicKey = (accountId: string): string => {
+  return (accountId && u8aToHex(decodeAddress(accountId))) || ''
 }
