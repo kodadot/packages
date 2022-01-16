4742d5bd16f8f2dc66dcebc696a28121af5283b5
Viki Val <viktorko99@gmail.com>
2022-01-16 11:38:00 +0100

    :zap: added upper trim with slugify

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,12 @@
+import slugify from 'slugify'
+
 export const trim = (text?: string) => (text || '').trim()
 
 export const trimAll = (text?: string) => (text || '').replace(/\s/g, "")
+
+const toSlug = (text: string, slug = '_'): string => slugify(text, slug)
+
+export const upperTrim = (name: string, shouldSlugify?: boolean): string => {
+  const result = trim(name).toUpperCase()
+  return shouldSlugify ? toSlug(result) : result
+}
