329084e923f0ec7d8a803ec4088e109874ef27e9
Viki Val <viktorko99@gmail.com>
2022-01-16 11:40:06 +0100

    :zap: added utils for create token and NFT

--- src/rmrk/create.ts
+++ src/rmrk/create.ts
@@ -1,4 +1,7 @@
-import { JustInteraction } from './types'
+import { isEmpty } from '../utils/empty'
+import { trim, upperTrim } from '../utils/string'
+import { toCollectionId, toSerialNumber } from './identification'
+import { CreatedCollection, CreatedNFT, JustInteraction, OnlyMintInteraction } from './types'
 
 export const createInteraction = (action: JustInteraction, version = '1.0.0', objectId: string, meta: string): string =>  {
   if (!objectId) {
@@ -6,4 +9,40 @@ export const createInteraction = (action: JustInteraction, version = '1.0.0', ob
   }
 
   return `RMRK::${action}::${version}::${objectId}${meta ? '::' + meta : ''}`
+}
+
+export const createMintInteaction = (action: OnlyMintInteraction, version = '1.0.0', object: CreatedNFT | CreatedCollection): string =>  {
+  if (isEmpty(object)) {
+    throw new ReferenceError(`[${action}] Could not create, because ${object} is empty`)
+  }
+
+  return `RMRK::${action}::${version}::${encodeURIComponent(
+    JSON.stringify(object)
+  )}`
+}
+
+export const createNFT = (caller: string, index: number, collectionId: string, name: string, metadata: string): CreatedNFT => {
+  const instance = upperTrim(name, true)
+  const sn = toSerialNumber(index)
+  return {
+    name: trim(name),
+    instance,
+    transferable: 1,
+    collection: collectionId,
+    sn,
+    metadata,
+    currentOwner: caller
+  }
+}
+
+export const createCollection = (caller: string, symbol: string, name: string, metadata: string, max = 0): CreatedCollection => {
+  const trimmedSymbol = upperTrim(symbol, true)
+  return {
+    id: toCollectionId(caller, trimmedSymbol),
+    symbol: trimmedSymbol,
+    issuer: caller,
+    name: trim(name),
+    max,
+    metadata,
+  }
 }
