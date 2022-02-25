02ecc70386d5744c34c4b07d680f76417a110d84
Viki Val <viktorko99@gmail.com>
2022-02-25 18:08:34 +0100

    :construction: implemented Subquery client

--- src/clients/SubqueryClient.ts
+++ src/clients/SubqueryClient.ts
@@ -1,14 +1,44 @@
 
-// import AbstractClient from './abstractClient'
-
 import build from '../queryBuilder'
-import { GraphQuery } from '../types'
-import { getFields } from './defaults'
+import { GraphQuery, ObjProp, BaseCollection, BaseNFT } from '../types'
 
-class SubqueryClient {
-  protected nftById (id: string, fields?: Array<keyof GraphQuery>): GraphQuery {
+import AbstractClient from './abstractClient'
+import { getFields, wrapSubqueryList } from './defaults'
+
+class SubqueryClient implements AbstractClient<BaseCollection, BaseNFT> {
+  nftById (id: string, fields?: ObjProp<BaseNFT>): GraphQuery {
     const toQuery = getFields(fields)
-    return build('nftById', toQuery, { id })
+    return build('nft: nFTEntity', toQuery, { id: { type: 'String', required: true, value: id, name: 'id' } })
+  }
+
+  nftListByOwner (owner: string, fields?: ObjProp<BaseNFT>): GraphQuery {
+    const toQuery = wrapSubqueryList(getFields(fields))
+    return build(`nfts: nFTEntities(filter: { currentOwner: { equalTo: ${owner} } })`, toQuery)
+  }
+
+  nftListByIssuer (issuer: string, fields?: ObjProp<BaseNFT>): GraphQuery {
+    const toQuery = wrapSubqueryList(getFields(fields))
+    return build(`nfts: nFTEntities(filter: { issuer: { equalTo: ${issuer} } })`, toQuery)
+  }
+
+  nftListByCollectionId (collectionId: string, fields?: ObjProp<BaseNFT>): GraphQuery {
+    const toQuery = wrapSubqueryList(getFields(fields))
+    return build(`nfts: nFTEntities(filter: { collectionId: { equalTo: ${collectionId} } })`, toQuery)
+  }
+
+  collectionById (id: string, fields?: ObjProp<BaseCollection>): GraphQuery {
+    const toQuery = getFields(fields)
+    return build('collection: collectionEntity', toQuery, { id: { type: 'String', required: true, value: id, name: 'id' } })
+  }
+
+  collectionListByOwner (owner: string, fields?: ObjProp<BaseCollection>): GraphQuery {
+    const toQuery = wrapSubqueryList(getFields(fields))
+    return build(`collections: collectionEntities(filter: { currentOwner: { equalTo: ${owner} } })`, toQuery)
+  }
+
+  collectionListByIssuer (issuer: string, fields?: ObjProp<BaseCollection>): GraphQuery {
+    const toQuery = wrapSubqueryList(getFields(fields))
+    return build(`collections: collectionEntities(filter: { issuer: { equalTo: ${issuer} } })`, toQuery)
   }
 }
 
