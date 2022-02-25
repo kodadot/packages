b3b34f56fd34769f0d9e34297ddd5cecdff5eaf2
Viki Val <viktorko99@gmail.com>
2022-02-25 17:02:11 +0100

    :zap: added wrapper for subquery lists

--- src/clients/defaults.ts
+++ src/clients/defaults.ts
@@ -1,6 +1,10 @@
-import { ObjProp } from '../types'
+import { ObjProp, Fields } from '../types'
 
 export const defaultField = ['id', 'metadata', 'currentOwner', 'issuer']
-export function getFields<T> (fields?: ObjProp<T>): ObjProp<T> | string[] {
+export function getFields<T> (fields?: ObjProp<T>): Fields<T> {
   return fields ?? defaultField
+}
+
+export function wrapSubqueryList<T> (fields: Fields<T>): [{ nodes: Fields<T> }] {
+  return [{ nodes: fields }]
 }
