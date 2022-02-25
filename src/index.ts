034cbf26465628d53b98cee637d8e12f06ba7745
Viki Val <viktorko99@gmail.com>
2022-02-25 18:40:16 +0100

    :tada: default export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export function testFunction (name: string) {
-  return `Hello ${name}`
-}
+import getClient from './clients/factory'
+import SquidClient from './clients/SquidClient'
+import SubqueryClient from './clients/SubqueryClient'
+
+export { SquidClient, SubqueryClient, getClient }
