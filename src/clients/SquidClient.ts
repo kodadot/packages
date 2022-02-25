1de57a03955f3cbcffd83a24675daad0fd9853b2
Viki Val <viktorko99@gmail.com>
2022-02-25 16:59:00 +0100

    :soap: fixed eslint errors

--- src/clients/SquidClient.ts
+++ src/clients/SquidClient.ts
@@ -16,7 +16,7 @@ class SquidClient implements AbstractClient<SquidCollection, SquidNFT> {
     return build(`nfts: nftEntities(where: {currentOwner_eq: ${owner}})`, toQuery)
   }
 
-  nftListByIssuer(issuer: string, fields?: ObjProp<SquidNFT>): GraphQuery {
+  nftListByIssuer (issuer: string, fields?: ObjProp<SquidNFT>): GraphQuery {
     const toQuery = getFields(fields)
     return build(`nfts: nftEntities(where: {issuer_eq: ${issuer}})`, toQuery)
   }
@@ -36,7 +36,7 @@ class SquidClient implements AbstractClient<SquidCollection, SquidNFT> {
     return build(`collections: collectionEntities(where: {currentOwner_eq: ${owner}})`, toQuery)
   }
 
-  collectionListByIssuer(issuer: string, fields?: ObjProp<SquidNFT>): GraphQuery {
+  collectionListByIssuer (issuer: string, fields?: ObjProp<SquidNFT>): GraphQuery {
     const toQuery = getFields(fields)
     return build(`collections: collectionEntities(where: {issuer_eq: ${issuer}})`, toQuery)
   }
