32b5e4d4895cfe18c78e2ee103b391d686d0670d
Viki Val <viktorko99@gmail.com>
2022-03-17 14:18:47 +0100

    :zap: use provider type instead of strings

--- src/clients/factory.ts
+++ src/clients/factory.ts
@@ -1,10 +1,11 @@
+import { Provider } from '../types'
 
 import SubqueryClient from './SubqueryClient'
 import SquidClient from './SquidClient'
 
 type Client = SquidClient | SubqueryClient
 
-function getClient (indexer?: 'subquery' | 'subsquid'): Client {
+function getClient (indexer?: Provider): Client {
   if (indexer === 'subquery') {
     return new SubqueryClient()
   }
