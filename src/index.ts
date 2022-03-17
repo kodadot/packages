ea3d5cd47bf0160077bc741d2ef4bf7a798afe65
Viki Val <viktorko99@gmail.com>
2022-03-17 14:19:18 +0100

    :zap: getUrl function to return graphql endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import getClient from './clients/factory'
 import SquidClient from './clients/SquidClient'
 import SubqueryClient from './clients/SubqueryClient'
+import getUrl from './indexers'
 
-export { SquidClient, SubqueryClient, getClient }
+export { SquidClient, SubqueryClient, getClient, getUrl }
