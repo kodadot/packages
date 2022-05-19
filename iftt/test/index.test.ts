9edb1abf49d6465c11edfb8641db4449a555be74
Viki Val <viktorko99@gmail.com>
2022-05-19 20:15:49 +0200

    :tada: :test_tube: first test

--- iftt/test/index.test.ts
+++ iftt/test/index.test.ts
@@ -1,15 +1,14 @@
 import { expect, it, describe } from 'vitest'
-import { testFunction } from '../src'
+import { IfThat, eq, build } from '../src'
+import { nftList, SomeNFT } from './data'
 
-describe('packageName', () => {
-  const tests = [
-    { input: 'foo', output: 'Hello foo' },
-    { input: 'bar', output: 'Hello bar' }
-  ]
-
-  for (const test of tests) {
-    it(test.input, () => {
-      expect(testFunction(test.input)).eq(test.output)
-    })
-  }
+describe('Welcome to the Black Parade', () => {
+  const nft = nftList[0]
+  it('sometimes I get the feeling she is watching over me', () => {
+    const newMeta = 'ipfs://ipfs/bafkreie6aheicniw2ene6iofxj7e6cettjioxojdstwabdqkxozjk2tyru'
+    const ifThat: IfThat<SomeNFT> = [eq(nft.currentOwner, nft.issuer), 'metadata', newMeta]
+    const res = build([ifThat], nft)
+    expect(res.metadata).eq(newMeta)
+    expect(nft.metadata).not.eq(res.metadata)
+  })
 })
