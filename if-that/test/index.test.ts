2ca854e0bd5d37c754af13b4c124877c6519fcb9
Viki Val <viktorko99@gmail.com>
2022-05-20 11:33:48 +0200

    :test_tube: can pass on multiple params

--- if-that/test/index.test.ts
+++ if-that/test/index.test.ts
@@ -1,10 +1,10 @@
 import { expect, it, describe } from 'vitest'
-import { IfThat, eq, build, gt } from '../src'
+import { IfThat, eq, build, gt, lte } from '../src'
 import { nftList, SomeNFT } from './data'
 
-describe('Builder', () => {
+describe('Builder should', () => {
   const nft = nftList[0]
-  it('should change metadata if data are equal', () => {
+  it('change metadata if data are equal', () => {
     const newMeta = 'ipfs://ipfs/bafkreie6aheicniw2ene6iofxj7e6cettjioxojdstwabdqkxozjk2tyru'
     const ifThat: IfThat<SomeNFT> = [eq(nft.currentOwner, nft.issuer), 'metadata', newMeta]
     const res = build([ifThat], nft)
@@ -12,10 +12,17 @@ describe('Builder', () => {
     expect(nft.metadata).not.eq(res.metadata)
   })
 
-  it('should remove metadata if data are greater than', () => {
+  it('remove metadata if data are greater than', () => {
     nft.previousOwnerCount = 6
     const ifThat: IfThat<SomeNFT> = [gt(nft.previousOwnerCount, 5), 'metadata', '']
     const res = build([ifThat], nft)
     expect(res.metadata).eq('')
+  })
+
+  it('change metadata if can pass multiple conditions', () => {
+    const newMeta = 'ipfs://ipfs/bafkreie6aheicniw2ene6iofxj7e6cettjioxojdstwabdqkxozjk2tyru'
+    const ifThat: IfThat<SomeNFT> = [[eq(nft.currentOwner, nft.issuer), lte(nft.royalty, 26)], 'metadata', newMeta]
+    const res = build([ifThat], nft)
+    expect(res.metadata).eq(newMeta)
   })
 })
