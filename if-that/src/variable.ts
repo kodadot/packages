6b8217e927916dda512e77b6f7ca980ff2851d06
Viki Val <viktorko99@gmail.com>
2022-05-28 21:30:55 +0200

    :zap: check if the key is nested

--- if-that/src/variable.ts
+++ if-that/src/variable.ts
@@ -3,3 +3,7 @@ import { Key, Variable } from './types'
 export function asVar<T>(value: Key<T>): Variable<T> {
   return { var: value }
 }
+
+export function isNestedKey<T>(value: Key<T>): boolean {
+  return String(value).includes('.')
+}
