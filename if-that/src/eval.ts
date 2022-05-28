d4f9e5dd4b757cd41dfa78b125459972d3b2229e
Viki Val <viktorko99@gmail.com>
2022-05-28 21:31:14 +0200

    :zap: applyAsNested

--- if-that/src/eval.ts
+++ if-that/src/eval.ts
@@ -1,5 +1,6 @@
 import { apply, RulesLogic } from 'json-logic-js'
 import { Key } from './types'
+import { isNestedKey } from './variable'
 
 export function evaluate<T>(condition: RulesLogic, entity: T): boolean {
   return apply(condition, entity)
@@ -10,5 +11,21 @@ export function applyResult<T>(result: boolean, key: Key<T>, value: any, entity:
     return entity
   }
 
+  if (isNestedKey(key)) {
+    return applyAsNested(key, value, entity)
+  }
+
   return { ...entity, [key]: value }
+}
+
+function applyAsNested<T>(key: Key<T>, value: any, entity: T): T {
+  const [parent, child] = String(key).split('.')
+  const parentValue = entity[parent]
+  return {
+    ...entity,
+    [parent]: {
+      ...parentValue,
+      [child]: value
+    }
+  }
 }
