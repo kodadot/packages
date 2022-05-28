edf5cfb99beb7610f8df7511a54161df2837da06
Viki Val <viktorko99@gmail.com>
2022-05-28 11:28:35 +0200

    :zap: ability to use variables

--- if-that/src/types.ts
+++ if-that/src/types.ts
@@ -4,6 +4,7 @@ export type Key<T> = keyof T
 export type OneOrMore<T> = T | T[]
 
 export type IfThat<T> = [OneOrMore<RulesLogic>, Key<T>, any]
+export type Variable<T> = { var: Key<T> }
 
 export type Operators =
   | '=='
