a602177d0072bd86332077d5cfffdccb46b8432b
Viki Val <viktorko99@gmail.com>
2022-05-28 21:28:19 +0200

    :zap: possilibity to use nested keys https://stackoverflow.com/a/68404823

--- if-that/src/types.ts
+++ if-that/src/types.ts
@@ -1,9 +1,15 @@
 import { RulesLogic } from 'json-logic-js'
 
+type DotPrefix<T extends string> = T extends '' ? '' : `.${T}`
+
+export type DotNestedKeys<T> = (T extends object ?
+    { [K in Exclude<keyof T, symbol>]: `${K}${DotPrefix<DotNestedKeys<T[K]>>}` }[Exclude<keyof T, symbol>]
+    : '') extends infer D ? Extract<D, string> : never;
+
 export type Key<T> = keyof T
 export type OneOrMore<T> = T | T[]
 
-export type IfThat<T> = [OneOrMore<RulesLogic>, Key<T>, any]
+export type IfThat<T> = [OneOrMore<RulesLogic>, DotNestedKeys<T>, any]
 export type Variable<T> = { var: Key<T> }
 
 export type Operators =
