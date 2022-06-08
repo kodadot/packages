1758682e766635cf75a69300459776f0193a9584
Viki Val <viktorko99@gmail.com>
2022-06-08 22:58:47 +0200

    :bug: fetch on wrong path

--- uniquery/src/rest/ask.ts
+++ uniquery/src/rest/ask.ts
@@ -4,10 +4,12 @@ import { pathToRequest } from './path'
 import { GraphRequest } from './types'
 import { getOptions } from './utils'
 
+const GRAPHQL_PATH = '/graphql'
+
 function askFor<T> (path: string): Promise<T> {
   const request = pathToRequest(path) as unknown as GraphRequest
   const options = getOptions(request)
-  return $fetch<T>(request.path, options)
+  return $fetch<T>(GRAPHQL_PATH, options)
 }
 
 export default askFor
