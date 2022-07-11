9ca5c649103d40203f0ecbc1ccf0f8826d305c7d
Viki Val <viktorko99@gmail.com>
2022-07-11 16:43:22 +0200

    :test_tube: useApi has some base tests

---
+++ sub-api/test/index.test.ts
@@ -0,0 +1,12 @@
+import { expect, it, describe } from 'vitest'
+import { useApi } from '../src/useApi'
+
+describe('SUB API', () => {
+  const url = 'https://moonriver.public.blastapi.io/'
+  // const url = 'https://kusama-rpc.polkadot.io'
+  it('Should return some data', async () => {
+    const api = await useApi(url)
+    const ss58 = api.consts.system.ss58Prefix.toString()
+    expect(ss58).eq('1285')
+  })
+})
