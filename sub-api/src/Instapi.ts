d6fe0870f0ad8e330de3aef9647ea86fb55bab93
Viki Val <viktorko99@gmail.com>
2022-07-14 18:50:36 +0200

    :zap: first version of instapi

---
+++ sub-api/src/Instapi.ts
@@ -0,0 +1,32 @@
+import { ApiPromise } from '@polkadot/api'
+import { getApiOptions } from './utils.js'
+import { ApiExtension } from './types.js'
+import { asWsProvider } from './helpers.js'
+
+/**
+ * Some wrapper Instance for @polkadot/api.
+ */
+class Api extends ApiPromise {
+  private _apiUrl: string
+
+  constructor(apiUrl: string, overrideOptions?: ApiExtension) {
+    const provider = asWsProvider(apiUrl)
+    const options = overrideOptions ?? getApiOptions(apiUrl)
+    super({ provider, ...options })
+    this.setUrl(apiUrl)
+  }
+
+  public getInstance(): Promise<ApiPromise> {
+    return this.isReady
+  }
+
+  private setUrl(apiUrl: string) {
+    this._apiUrl = apiUrl
+  }
+
+  get url() {
+    return this._apiUrl
+  }
+}
+
+export default Api
