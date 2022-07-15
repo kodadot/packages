3ee5eb5a6515a92d9f0e41286b8ae037c0ba74b4
Viki Val <viktorko99@gmail.com>
2022-07-15 23:02:33 +0200

    :zap: Api will disconnect when there is error

--- sub-api/src/instantapi.ts
+++ sub-api/src/instantapi.ts
@@ -2,6 +2,7 @@ import { ApiPromise } from '@polkadot/api'
 import { getApiOptions } from './utils.js'
 import { ApiExtension } from './types.js'
 import { asWsProvider } from './helpers.js'
+import { DETATCH_IN } from './constants'
 
 /**
  * Some wrapper Instance for @polkadot/api.
@@ -14,10 +15,21 @@ class InstantApi extends ApiPromise {
     const options = overrideOptions ?? getApiOptions(apiUrl)
     super({ provider, ...options })
     this.setUrl(apiUrl)
+    this.once('error', () => {
+      console.warn('[KODADOT::SUBAPI] WARN: Unable to init api with apiUrl', apiUrl)
+      this.detach()
+    })
   }
 
   public getInstance(): Promise<ApiPromise> {
-    return this.isReady
+    return this.isReadyOrError
+  }
+
+  private detach() {
+    console.log(`[KODADOT::SUBAPI] WARN: Detaching api in ${DETATCH_IN} ms`)
+    setTimeout(() => {
+      this.disconnect()
+    }, DETATCH_IN)
   }
 
   private setUrl(apiUrl: string) {
