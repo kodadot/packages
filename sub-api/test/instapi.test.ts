b55f4197533e43c195e4b87ff8f8ec3077a64858
Viki Val <viktorko99@gmail.com>
2022-07-15 22:46:19 +0200

    :test_tube: write test for smarter disconnecting

--- sub-api/test/instapi.test.ts
+++ sub-api/test/instapi.test.ts
@@ -3,11 +3,23 @@ import Api from '../src/instantapi'
 import { Endpoint } from './dummy'
 
 describe('Custom instance of ApiPromise', () => {
-  const url = Endpoint.KSM
-  it('Should return some data', async () => {
+  it.skip('Should return some data', async () => {
+    const url = Endpoint.KSM
     const api = await new Api(url).getInstance()
     const ss58 = api.consts.system.ss58Prefix.toString()
     expect(ss58).eq('2')
     api.disconnect()
   })
+
+  it('Should throw when not connected', async () => {
+    const url = Endpoint.LOCAL
+    try {
+      const api = await new Api(url).getInstance()
+      const ss58 = api.consts.system.ss58Prefix.toString()
+      expect(ss58).eq('42')
+      api.disconnect()
+    } catch (e) {
+      expect(e.message).eq('Not connected')
+    }
+  }, 10000)
 })
