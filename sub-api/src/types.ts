9c535e5c0e9fbfcdcbe138e1cd2cde17e4bc6388
Viki Val <viktorko99@gmail.com>
2022-07-18 11:53:30 +0200

    :label: types for the extrinsics

--- sub-api/src/types.ts
+++ sub-api/src/types.ts
@@ -1,5 +1,9 @@
 
+import { SubmittableExtrinsic } from '@polkadot/api/submittable/types'
 import type { RegistryTypes } from '@polkadot/types-codec/types'
 
 export type ApiExtensionMap = Record<string, ApiExtension>
 export type ApiExtension = { types?: RegistryTypes }
+export type ExtrinsicFunction<T> = (arg: T) => Extrinsic
+
+export type Extrinsic = SubmittableExtrinsic<'promise'>
