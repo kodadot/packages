3d2301273e8fc676bb1077d0f41c537638b49eac
Viki Val <viktorko99@gmail.com>
2022-07-20 17:16:45 +0200

    :zap: export directly query utils

--- sub-api/src/index.ts
+++ sub-api/src/index.ts
@@ -1,10 +1,12 @@
 // https://polkadot.js.org/docs/api/FAQ#since-upgrading-to-the-7x-series-typescript-augmentation-is-missing
-import '@polkadot/api-augment'
+// import '@polkadot/api-augment'
 
 import Api from './Api'
 import InstantApi from './instantapi'
 import ApiFactory from './ApiFactory'
 import onApiConnect from './onApiConnect'
+export * from './common/extrinsics'
+export * from './common/query'
 export * from './types'
 
 export { InstantApi, ApiFactory, onApiConnect }
