ade00fcb60f66b845b451f47c4e8d5c549b0d1e6
Junwei Chen <kngzhi@hotmail.com>
2022-08-29 11:25:00 +0800

    feat: colleciton and nft types

--- minimark/src/rmrk/v2/types.ts
+++ minimark/src/rmrk/v2/types.ts
@@ -78,3 +78,35 @@ export type InteractionV2Value = Base | Equip | Equippable | Lock | Resadd
     | SetPriority | SetProperty | AddTheme | undefined
     | BUY | EMOTE | SEND | LIST | CHANGEISSUER
     | CREATE | MINT | BURN
+
+export interface CreatedCollectionV2 {
+    max: number
+    issuer: string
+    symbol: string
+    id: string;
+    metadata: string
+    properties?: IProperties
+}
+
+// from https://github.com/rmrk-team/rmrk-spec/blob/master/standards/rmrk2.0.0/entities/collection.md#properties-format
+export type IProperties = Record<string, IAttribute>;
+
+export interface IAttribute {
+    _mutation?: {
+        allowed: boolean;
+        with?: {
+            opType: InteractionV2Type;
+            condition?: string;
+        };
+    };
+    type: "array" | "object" | "int" | "float" | "boolean" | "datetime" | "string";
+    value: any;
+}
+
+export interface CreatedNFTV2 {
+  collection: string
+  metadata: string
+  sn: string
+  transferable: number
+  symbol: string
+}
