d2788f243d7c1bdfe8d192e0ea5700c8c86dc5b0
Viki Val <viktorko99@gmail.com>
2022-08-31 12:43:42 +0200

    :label: extract types

--- metasquid/src/types.ts
+++ metasquid/src/types.ts
@@ -5,3 +5,54 @@ export type EntityWithId = {
 export type BurnableEntity = EntityWithId & {
   burned: boolean;
 }
+
+// shared
+export type BaseCall = {
+  caller: string;
+  blockNumber: string;
+  timestamp: Date;
+};
+
+export type IEvent<T> = BaseCall & {
+  interaction: T;
+  blockNumber: bigint,
+  currentOwner: string,
+  meta: string;
+}
+
+// typeorm
+export type EntityConstructor<T> = {
+  new (...args: any[]): T;
+};
+
+
+// meta:
+export type Optional<T> = T | null;
+export type CallWith<T> = BaseCall & T;
+
+
+// entity
+export type TokenMetadata = {
+  name?: string
+  description: string
+  external_url?: string
+  image: string
+  animation_url?: string
+  attributes?: MetadataAttribute[]
+  mediaUri?: string;
+  type?: string;
+  thumbnailUri?: string;
+}
+
+export type MetadataAttribute = {
+  display_type?: DisplayType
+  trait_type?: string
+  value: number | string
+};
+
+export enum DisplayType {
+  null,
+  'boost_number',
+  'number',
+  'boost_percentage',
+}
