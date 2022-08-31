efa110a78c4699fd75426faa4f2eb37679612308
Viki Val <viktorko99@gmail.com>
2022-08-31 15:23:14 +0200

    :zap: typeorm utils

---
+++ metasquid/src/entity.ts
@@ -0,0 +1,65 @@
+import { Store, EntityConstructor } from './types'
+import { FindOptionsWhere } from 'typeorm'
+
+export type EntityWithId = {
+  id: string
+}
+
+export async function createOrElseThrow<T extends EntityWithId>(
+  store: Store,
+  entityConstructor: EntityConstructor<T>,
+  id: string,
+  init: Partial<T>
+): Promise<T> {
+    const entity = await get(store, entityConstructor, id)
+    if (entity) {
+        throw new Error(`Entity with id ${id} already exists`)
+    }
+
+    return create(entityConstructor, id, init)
+}
+
+/**
+ * Get or Create the provided entity with the given ID
+ *
+ * Note: you need to persist/save the entity yourself
+ */
+export async function getOrCreate<T extends EntityWithId>(
+  store: Store,
+  entityConstructor: EntityConstructor<T>,
+  id: string,
+  init: Partial<T>
+): Promise<T> {
+  // attempt to get the entity from the database
+  let entity = await get(store, entityConstructor, id)
+
+  // if the entity does not exist, construct a new one
+  // and assign the provided ID to it
+  if (entity == null) {
+    entity = new entityConstructor()
+    entity.id = id
+    Object.assign(entity, init)
+  }
+
+  return entity
+}
+
+export function get<T extends EntityWithId>(
+  store: Store,
+  entityConstructor: EntityConstructor<T>,
+  id: string
+): Promise<T | null> {
+  const where: FindOptionsWhere<T> = { id } as FindOptionsWhere<T>
+  return store.findOneBy<T>(entityConstructor, where)
+}
+
+export function create<T extends EntityWithId>(
+  entityConstructor: EntityConstructor<T>,
+  id: string,
+  init: Partial<T>
+) {
+  const entity = new entityConstructor()
+  entity.id = id
+  Object.assign(entity, init)
+  return entity
+}
