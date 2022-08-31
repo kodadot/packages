78433be967f928930b2d0df083ccad3f43f734dd
Viki Val <viktorko99@gmail.com>
2022-08-31 17:10:45 +0200

    :squid: very specific squid utils

--- metasquid/src/specific.ts
+++ metasquid/src/specific.ts
@@ -1,10 +1,24 @@
-// let prefix: string = 'kusama'
+import * as ss58 from '@subsquid/ss58'
+import { decodeHex } from '@subsquid/substrate-processor'
 
-// const SquidUtils = (chain?: string) => {
-//   prefix = chain
-// }
+let prefix: string = 'kusama'
 
-// export { SquidUtils }
+function addressOf(address: Uint8Array | string): string {
+  const value = typeof address === 'string' ? decodeHex(address) : address
+  if (!value) {
+    return ''
+  }
+  return ss58.codec(prefix).encode(value)
+}
+
+const SquidUtils = (chain?: string) => {
+  prefix = chain ?? prefix
+  return {
+    addressOf
+  }
+}
+
+export { SquidUtils }
 
 // Usage:
 // import { SquidUtils } from '@kodadot1/metasquid'
