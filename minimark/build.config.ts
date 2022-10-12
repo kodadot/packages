aedb1d1e81642992cce58510363b7cda39059b9f
Viki Val <viktorko99@gmail.com>
2022-10-12 15:26:39 +0200

    :wrench: we are getting to the correct config

--- minimark/build.config.ts
+++ minimark/build.config.ts
@@ -4,16 +4,32 @@ export default defineBuildConfig({
   // If entries is not provided, will be automatically inferred from package.json
   entries: [
     // default
-    './src/index',
-    // mkdist builder transpiles file-to-file keeping original sources structure
-    {
-      input: './src/rmrk/',
-      outDir: './dist/v1'
-    }
+    'src/index'
+    // this is how to make whole package tree available
+    // {
+    //   input: './src/rmrk/',
+    //   outDir: './dist/v1'
+    // },
   ],
   outDir: 'dist',
+  clean: true,
   declaration: true,
   rollup: {
     emitCJS: true
   }
 })
+
+// Subpath import in package.json
+// {
+//   input: 'src/common/index',
+//   name: 'common'
+// }
+// {
+//   "exports": {
+//     "./common": {
+//       "types": "./dist/common.d.ts",
+//       "import": "./dist/common.mjs",
+//       "require": "./dist/common.cjs"
+//     }
+//   }
+// }
