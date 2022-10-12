61752eff8bef9b0aac5354475e6677c0f81df4dd
Branislav Hozza <brankohozza@gmail.com>
2022-10-12 18:41:39 +0200

    :construction: fix: move functionality to correct package

--- minimark/src/utils/ipfs.ts
+++ minimark/src/utils/ipfs.ts
@@ -20,14 +20,3 @@ export const justHash = (ipfsLink?: string): boolean => {
 export const unSanitizeIpfsUrl = (url: string): string => {
   return justHash(url) ? addPrefix(url) : url
 }
-
-export const DEFAULT_PROVIDER_LIST = [' https://cloudflare-ipfs.com', 'https://kodadot.mypinata.cloud', 'https://nftstorage.link/']
-export type ProviderUrl = typeof DEFAULT_PROVIDER_LIST[number]
-
-export const ipfsGet = <T extends Response>(url: string, providers: ProviderUrl[] = DEFAULT_PROVIDER_LIST): Promise<T> => {
-  const [hash] = (url.match(/^(ipfs:\/\/)*(ipfs\/)*([a-zA-Z0-9]+)$/) as RegExpMatchArray)?.reverse() ?? []
-  if (!hash) {
-    throw new TypeError(`Invalid IPFS URL: ${url}`)
-  }
-  return Promise.race<T>(providers.map<Promise<T>>(provider => fetch(`${provider}/ipfs/${hash}`) as Promise<T>))
-}
--- minipfs/src/race.ts
+++ minipfs/src/race.ts
@@ -7,3 +7,28 @@
 // }
 
 // const getSingle = <T>(url: string, baseURL: string): Promise<T> => $fetch<T>(url, { baseURL })
+
+export const DEFAULT_PROVIDER_LIST = [
+  ' https://cloudflare-ipfs.com',
+  'https://kodadot.mypinata.cloud',
+  'https://nftstorage.link/'
+]
+export type ProviderUrl = typeof DEFAULT_PROVIDER_LIST[number];
+
+export const ipfsGet = <T extends Response>(
+  url: string,
+  providers: ProviderUrl[] = DEFAULT_PROVIDER_LIST
+): Promise<T> => {
+  const [hash] =
+    (
+      url.match(/^(ipfs:\/\/)*(ipfs\/)*([a-zA-Z0-9]+)$/) as RegExpMatchArray
+    )?.reverse() ?? []
+  if (!hash) {
+    throw new TypeError(`Invalid IPFS URL: ${url}`)
+  }
+  return Promise.race<T>(
+    providers.map<Promise<T>>(
+      provider => fetch(`${provider}/ipfs/${hash}`) as Promise<T>
+    )
+  )
+}
