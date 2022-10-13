cb81c20236c36c799a9a26b733e1d0a1b589f2e1
Viki Val <viktorko99@gmail.com>
2022-10-13 12:04:57 +0200

    :truck: named exports only

--- minimark/src/rmrk/v2/unwrap.ts
+++ minimark/src/rmrk/v2/unwrap.ts
@@ -4,13 +4,13 @@ import { isRemark, splitBySquare, toVersion } from '../helpers'
 import { toInteractionV2, resolveRmrk2Value } from './helper'
 import { UnwrappedRemark2, InteractionV2Value } from './types'
 
-const unwrapV2 = <T = InteractionV2Value>(text: string): UnwrappedRemark2<T | InteractionV2Value> => {
+const unwrap = <T = InteractionV2Value>(text: string): UnwrappedRemark2<T | InteractionV2Value> => {
   const decoded = unwrapURI(decodeHex(text))
 
   if (!isRemark(decoded)) {
     throw new TypeError(`RMRK: Unable to unwrap object ${decoded}`)
   }
-  const [_, mayInteraction, mayVersion, id, ...rest] = splitBySquare(decoded)
+  const [, mayInteraction, mayVersion, id, ...rest] = splitBySquare(decoded)
 
   const interaction = toInteractionV2(mayInteraction)
   const value = resolveRmrk2Value(interaction, id, rest)
@@ -22,4 +22,4 @@ const unwrapV2 = <T = InteractionV2Value>(text: string): UnwrappedRemark2<T | In
   }
 }
 
-export default unwrapV2
+export { unwrap as unwrapRemarkV2 }
