a312c39f0952b0423c0ce928e0f1ad551fb978b2
Viki Val <viktorko99@gmail.com>
2022-10-18 15:20:05 +0200

    :label: defined HTTPS_URI

--- minipfs/src/types.ts
+++ minipfs/src/types.ts
@@ -1,2 +1,6 @@
 export type Callback<T = any, V = any> = (param: T) => Promise<V>
 export type UnwrapCallback<T> = (param: T) => string
+
+// Fancy
+export type HTTPS_URI = `https://${string}/`
+export type IPFS_URI = `ipfs://${string}`
