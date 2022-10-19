95e9cb5337b609d0938d55dbd5760283e5c0ea7a
Viki Val <viktorko99@gmail.com>
2022-10-19 15:35:10 +0200

    :zap: obtain functions

--- minipfs/src/obtain.ts
+++ minipfs/src/obtain.ts
@@ -1,5 +1,15 @@
+/* eslint-disable no-console */
+import { $fetch } from 'ohmyfetch'
 import { URI } from './types'
 
-export function $obtain(uri: URI) {
+export function obtain<T>(uri: URI): Promise<T> {
+  return $fetch<T>(uri)
+    .catch((err) => {
+      console.error(err)
+      return {} as T
+    })
+}
 
+export function obtainMedia(uri: URI): Promise<Blob> {
+  return $fetch(uri, { responseType: 'blob' })
 }
