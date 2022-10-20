ba0a65799b8820de571e9d88adab16845ef86310
Viki Val <viktorko99@gmail.com>
2022-10-20 18:12:43 +0200

    :test_tube: test sanitizer

---
+++ minipfs/test/sanitize.test.ts
@@ -0,0 +1,17 @@
+import { expect, it, describe } from 'vitest'
+import { sanitize } from '../src'
+
+describe('Sanitize', () => {
+  const tests = [
+    { input: 'bafkreiazeqysfmeuzqcnjp6rijxfu5h7sj3t4h2rxehi7rlyegzfy7lxeq', output: '/ipfs/bafkreiazeqysfmeuzqcnjp6rijxfu5h7sj3t4h2rxehi7rlyegzfy7lxeq' },
+    { input: '/ipfs/bafkreiazeqysfmeuzqcnjp6rijxfu5h7sj3t4h2rxehi7rlyegzfy7lxeq', output: '/ipfs/bafkreiazeqysfmeuzqcnjp6rijxfu5h7sj3t4h2rxehi7rlyegzfy7lxeq' },
+    { input: 'ipfs://bafybeiennmmolv3keadouvgrhqkkfhprobvikl6hmblg4x2nxsn7s7lrjq', output: '/ipfs/bafybeiennmmolv3keadouvgrhqkkfhprobvikl6hmblg4x2nxsn7s7lrjq' },
+    { input: 'ipfs://ipfs/bafybeiennmmolv3keadouvgrhqkkfhprobvikl6hmblg4x2nxsn7s7lrjq', output: '/ipfs/bafybeiennmmolv3keadouvgrhqkkfhprobvikl6hmblg4x2nxsn7s7lrjq' }
+  ]
+
+  for (const test of tests) {
+    it(test.input, () => {
+      expect(sanitize(test.input).path).eq(test.output)
+    })
+  }
+})
