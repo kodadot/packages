26a86c52f5f1c8d2e289e18ee2b91ffb0fde199d
Viki Val <viktorko99@gmail.com>
2022-10-20 16:16:22 +0200

    :soap: cleanup and stuff

--- minipfs/src/index.ts
+++ minipfs/src/index.ts
@@ -1,18 +1,9 @@
-import { AvailableProviders } from './gateways'
-import { obtain, obtainSafe } from './obtain'
-import { competition } from './race'
-import { sanitize } from './sanitize'
-import { IPFS_PATH, IPNS_PATH, SanitizedOutput, URI } from './types'
-
+export * from './arweave'
+export * from './cid'
+export * from './constants'
+export * from './gateways'
+export * from './magic'
 export * from './obtain'
-
-export function $obtain<T>(uri: URI, providers: AvailableProviders = [], safe: boolean = true): Promise<T> {
-  const { needProvider, path }: SanitizedOutput = sanitize(uri)
-  const callback = safe ? obtainSafe : obtain
-
-  if (needProvider) {
-    return competition<T>(path as IPFS_PATH | IPNS_PATH, providers, callback)
-  }
-
-  return callback(path)
-}
+export * from './race'
+export * from './sanitize'
+export * from './types'
