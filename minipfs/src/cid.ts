bda5ff38de6546345b6ba883dc07634f945da830
Viki Val <viktorko99@gmail.com>
2022-10-20 15:24:56 +0200

    :zap: CID is now failsafe

--- minipfs/src/cid.ts
+++ minipfs/src/cid.ts
@@ -1,5 +1,7 @@
-import { cid, path } from 'is-ipfs'
+import { cid, path, url } from 'is-ipfs'
 import { HTTP_REGEX } from './constants'
+import { PINATA_GATEWAY } from './gateways'
+import { IPFS_HASH, IPFS_PATH, IPNS_PATH } from './types'
 
 export const isCID = (uri: string): boolean => {
   return cid(uri)
@@ -13,20 +15,22 @@ export const isPath = (uri: string): boolean => {
   return path(uri)
 }
 
-export const toIPFSPath = (uri: string): string => {
+export const toIPFSPath = (uri: string): IPFS_HASH | IPFS_PATH | IPNS_PATH => {
   if (isCID(uri)) {
     return `/ipfs/${uri}`
   }
 
   if (isPath(uri)) {
-    return uri
+    return uri as IPFS_PATH | IPNS_PATH
   }
 
   throw new TypeError(`Invalid IPFS URI: ${uri}`)
 }
 
-// Exported from kodadot
-// export const isIpfsCid = (url: stng): boolean => {
-//   return /^[0-9a-zA-Z]{44,}$/.test(url)
-// }
+export function isDefaultPinataProvider(uri: string): boolean {
+  return uri.startsWith(PINATA_GATEWAY) && url(uri)
+}
 
+export const canBeIPFS = (uri: string): boolean => {
+  return isCID(uri) || isPath(uri)
+}
