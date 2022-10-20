db14829785945071fb1594f442fedc1b98a67248
Viki Val <viktorko99@gmail.com>
2022-10-20 16:29:20 +0200

    :zap: use string otherwise is unusable

--- minipfs/src/magic.ts
+++ minipfs/src/magic.ts
@@ -4,7 +4,7 @@ import { competition } from './race'
 import { sanitize } from './sanitize'
 import { IPFS_PATH, IPNS_PATH, SanitizedOutput, URI } from './types'
 
-export function $obtain<T>(uri: URI, providers: AvailableProviders = [], safe: boolean = true): Promise<T> {
+export function $obtain<T>(uri: URI | string, providers: AvailableProviders = [], safe: boolean = true): Promise<T> {
   const { needProvider, path }: SanitizedOutput = sanitize(uri)
   const callback = safe ? obtainSafe : obtain
 
