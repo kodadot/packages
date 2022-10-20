95c8415b812a68ab1e2e61af9e469001e809ee17
Viki Val <viktorko99@gmail.com>
2022-10-20 16:44:38 +0200

    :zap: more ipfs prefixes

--- minipfs/src/constants.ts
+++ minipfs/src/constants.ts
@@ -1,4 +1,6 @@
-export const IPFS_PREFIX = 'ipfs://ipfs/'
+export const IPFS_FS_PREFIX = 'ipfs://ipfs/'
+export const IPFS_NS_PREFIX = 'ipfs://ipns/'
+export const IPFS_PREFIX = 'ipfs://'
 
 // regex
 export const HTTP_REGEX = /^https?:\/\//
