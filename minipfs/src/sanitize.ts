ed08fe86c4127160a951e5c172afa22341c472f5
Viki Val <viktorko99@gmail.com>
2022-10-20 16:45:27 +0200

    :bug: sanitize can now clear it

--- minipfs/src/sanitize.ts
+++ minipfs/src/sanitize.ts
@@ -1,12 +1,16 @@
 import { toArweavePath } from './arweave'
-import { canBeIPFS, isDefaultPinataProvider, toIPFSPath } from './cid'
+import { canBeIPFS, extractIPFS, isDefaultPinataProvider, toIPFSPath } from './cid'
 import { ARWEAVE_REGEX, HTTP_REGEX, IPFS_REGEX } from './constants'
 import { PINATA_GATEWAY } from './gateways'
 import { HTTPS_URI, IPFS_PATH, IPNS_PATH, SanitizedOutput } from './types'
 
 export function sanitize(path: string): SanitizedOutput {
-  if (IPFS_REGEX.test(path) || canBeIPFS(path)) {
+  if (canBeIPFS(path)) {
     return { path: toIPFSPath(path), needProvider: true }
+  }
+
+  if (IPFS_REGEX.test(path)) {
+    return { path: toIPFSPath(extractIPFS(path)), needProvider: true }
   }
 
   if (HTTP_REGEX.test(path)) {
