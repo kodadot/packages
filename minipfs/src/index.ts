13aef83302aa964a0ff925c6667d1538c53faf4d
Viki Val <viktorko99@gmail.com>
2022-10-20 16:01:24 +0200

    :zap: default obtain fn

--- minipfs/src/index.ts
+++ minipfs/src/index.ts
@@ -1,3 +1,18 @@
-export function testFunction (name: string) {
-  return `Hello ${name}`
+import { AvailableProviders } from './gateways'
+import { obtain, obtainSafe } from './obtain'
+import { competition } from './race'
+import { sanitize } from './sanitize'
+import { IPFS_PATH, IPNS_PATH, SanitizedOutput, URI } from './types'
+
+export * from './obtain'
+
+export function $obtain<T>(uri: URI, providers: AvailableProviders = [], safe: boolean = true): Promise<T> {
+  const { needProvider, path }: SanitizedOutput = sanitize(uri)
+  const callback = safe ? obtainSafe : obtain
+
+  if (needProvider) {
+    return competition<T>(path as IPFS_PATH | IPNS_PATH, providers, callback)
+  }
+
+  return callback(path)
 }
