9d6683080784b94191857657ad8488530df7e5cc
Viki Val <viktorko99@gmail.com>
2022-11-01 20:23:25 +0100

    :truck: orderiiing

--- uniquery/src/clients/abstractClient.ts
+++ uniquery/src/clients/abstractClient.ts
@@ -1,8 +1,9 @@
 
-import { ObjProp, GraphQuery, QueryOptions, BaseEvent } from '../types'
+import { ObjProp, GraphQuery, QueryOptions, BaseEvent, KeyOf } from '../types'
 // Collection, Token
 interface AbstractClient<C, T, E = BaseEvent> {
   collectionById(id: string, fields?: ObjProp<C>): GraphQuery
+  // collectionListBy(id: string, field: KeyOf<C>, fields?: ObjProp<C>): GraphQuery
   collectionListByIssuer(issuer: string, fields?: ObjProp<C>, options?: QueryOptions): GraphQuery
   collectionListByOwner(owner: string, fields?: ObjProp<C>, options?: QueryOptions): GraphQuery
   eventListByAddress(address: string, fields?: ObjProp<E>, options?: QueryOptions): GraphQuery
@@ -11,6 +12,7 @@ interface AbstractClient<C, T, E = BaseEvent> {
   eventListByInteraction(interaction: string, fields?: ObjProp<E>, options?: QueryOptions): GraphQuery
   eventListByNftId(id: string, fields?: ObjProp<E>, options?: QueryOptions): GraphQuery
   nftById(id: string, fields?: ObjProp<T>): GraphQuery
+  // nftListBy(id: string, field: KeyOf<T>, fields?: ObjProp<T>): GraphQuery
   nftListByCollectionId(collectionId: string, fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
   nftListByCollectionIdAndOwner(id: string, owner: string, fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
   nftListByCollectionIdList(ids: string[], fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
@@ -20,8 +22,6 @@ interface AbstractClient<C, T, E = BaseEvent> {
   nftListCollectedBy(address: string, fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
   nftListForSale(fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
   nftListSoldBy(address: string, fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
-  // collectionListBy(id: string, field: KeyOf<C>, fields?: ObjProp<C>): GraphQuery
-  // nftListBy(id: string, field: KeyOf<T>, fields?: ObjProp<T>): GraphQuery
   // collectionStatListById(id: string, fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
   // lastNftIdbyCollectionId(id: string, fields?: ObjProp<T>, options?: QueryOptions): GraphQuery
 }
