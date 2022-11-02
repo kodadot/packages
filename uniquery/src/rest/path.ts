7ed49ace263bb7e9a2bf0c1fa36b398b7f1b9837
Viki Val <viktorko99@gmail.com>
2022-11-02 20:26:48 +0100

    :zap: first shot for impl

--- uniquery/src/rest/path.ts
+++ uniquery/src/rest/path.ts
@@ -1,5 +1,8 @@
 import { $URL, withoutLeadingSlash } from 'ufo'
-import { ClientCall, GraphRequest } from './types'
+import getClient from '../clients/factory'
+import { GraphQuery } from '../types'
+import { getUrl } from './indexers'
+import { ClientCall, GraphRequest, Prefix } from './types'
 import validate from './validation'
 
 const pathMap: Record<string, ClientCall> = {
@@ -21,12 +24,20 @@ export const parsePath = (pathname: string): string[] => {
 
 const hasCall = (call: string | undefined): boolean => call in pathMap
 const supportChain = (chain: string | undefined): boolean => {
-  // getUrl(chain)
-  console.log(chain)
-  return true
+  const uri = getUrl((chain as Prefix) || '')
+  return !!uri
 }
 
 const urlOf = (path: string): $URL => new $URL(path)
+const makeQuery = (call: string, id: string): GraphQuery => {
+  const client = getClient()
+  const fn: ((id: string) => GraphQuery) | undefined = client[call]
+  if (fn === undefined || !(typeof fn === 'function')) {
+    throw new ReferenceError(`[UNIQUERY::REST] Unable to make call: ${call}`)
+  }
+
+  return fn(id)
+}
 
 // /bsx/nft/:id
 // TODO: should return GraphRequest
@@ -36,9 +47,12 @@ export function pathToRequest(path: string): GraphRequest {
   validate(chain, call, id)
   console.log(query)
   if (!hasCall(call) || !supportChain(chain)) {
-    throw new ReferenceError('Invalid path')
+    throw new ReferenceError(`[UNIQUERY::REST] Invalid path: ${path}`)
   }
 
-  return {} as GraphRequest
+  const baseURL = getUrl(chain as Prefix)
+  const graphQuery = makeQuery(call, id)
+
+  return { baseURL, query: graphQuery, path }
   // const [chain, call, param] = path.split('/').filter(Boolean)
 }
