defe8589a3604710cb98bd2d1990bbecbb651c55
Viki Val <viktorko99@gmail.com>
2022-11-02 14:39:38 +0100

    :zap: metadata should be findeable by IPFS CID

--- uniquery/src/clients/abstractClient.ts
+++ uniquery/src/clients/abstractClient.ts
@@ -18,6 +18,7 @@ interface AbstractClient<C, T, E = BaseEvent> {
   nftListByCollectionIdList(ids: string[], options?: QueryProps<T>): GraphQuery
   nftListByIssuer(issuer: string, options?: QueryProps<T>): GraphQuery
   nftListByMetadataId(id: string, options?: QueryProps<T>): GraphQuery
+  nftListByMetadataIdMatch(id: string, options?: QueryProps<T>): GraphQuery
   nftListByOwner(owner: string, options?: QueryProps<T>): GraphQuery
   nftListCollectedBy(address: string, options?: QueryProps<T>): GraphQuery
   nftListForSale(options?: QueryProps<T>): GraphQuery
