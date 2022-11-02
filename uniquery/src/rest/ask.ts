53e8295336bc439fc034249c5e8e5c6c19c1cccf
Viki Val <viktorko99@gmail.com>
2022-11-02 19:37:57 +0100

    :label: ask returns GraphQL request

--- uniquery/src/rest/ask.ts
+++ uniquery/src/rest/ask.ts
@@ -3,14 +3,15 @@
 // unwrap data should be done in the caller
 import { $fetch } from 'ohmyfetch'
 import { pathToRequest } from './path'
+import { GraphLike } from './types'
 import { getOptions } from './utils'
 
 const GRAPHQL_PATH = '/graphql'
 
-function askFor<T>(path: string): Promise<T> {
+function askFor<T>(path: string): Promise<GraphLike<T>> {
   const request = pathToRequest(path)
   const options = getOptions(request)
-  return $fetch<T>(GRAPHQL_PATH, options)
+  return $fetch<GraphLike<T>>(GRAPHQL_PATH, options)
 }
 
 export { askFor as ask }
