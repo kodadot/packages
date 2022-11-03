7a962274badc71f338866fbd6ae3f53182613ce4
Viki Val <viktorko99@gmail.com>
2022-11-03 11:58:24 +0100

    :test_tube: fixd base tests

--- uniquery/test/index.test.ts
+++ uniquery/test/index.test.ts
@@ -5,16 +5,16 @@ describe('KODAPI UTILS', () => {
   describe('getURL', () => {
     it('should return default kusama indexer', () => {
       const url = getUrl('kusama')
-      expect(url).eq('https://kodadot.api.subquery.network')
+      expect(url).eq('https://squid.subsquid.io/rubick/v/007/graphql')
     })
 
-    it('should return subquery kusama indexer', () => {
-      const url = getUrl('kusama', 'subquery')
-      expect(url).eq('https://kodadot.api.subquery.network')
+    it('should return subsquid kusama indexer', () => {
+      const url = getUrl('kusama', 'subsquid')
+      expect(url).eq('https://squid.subsquid.io/rubick/v/007/graphql')
     })
 
-    it('should throw on subsquid kusama indexer', () => {
-      const fn = () => getUrl('kusama', 'subsquid')
+    it('should throw on subquery kusama indexer', () => {
+      const fn = () => getUrl('kusama', 'subquery')
       expect(fn).toThrow(ReferenceError)
     })
   })
