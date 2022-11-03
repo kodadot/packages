cb925b99b5f6ca6636b63b7fa0a507f4a255e331
Viki Val <viktorko99@gmail.com>
2022-11-03 12:07:59 +0100

    :sparkles: magick makeQuery

--- uniquery/src/rest/path.ts
+++ uniquery/src/rest/path.ts
@@ -6,16 +6,20 @@ import { ClientCall, GraphRequest, Prefix } from './types'
 import validate from './validation'
 
 const pathMap: Record<string, ClientCall> = {
-  nft: 'nftById',
   collection: 'collectionById',
-  nftByOwner: 'nftListByOwner',
-  nftByIssuer: 'nftListByIssuer',
-  nftListCollectedBy: 'nftListCollectedBy',
-  nftListSoldBy: 'nftListSoldBy',
-  nftByCollection: 'nftListByCollectionId',
-  nftForSale: 'nftListForSale',
   collectionByOwner: 'collectionListByOwner',
-  collectionByIssuer: 'collectionListByIssuer'
+  collectionByIssuer: 'collectionListByIssuer',
+  eventByAddress: 'eventListByAddress',
+  eventByInteraction: 'eventListByInteraction',
+  eventByNftId: 'eventListByNftId',
+  nft: 'nftById',
+  nftByCollection: 'nftListByCollectionId',
+  nftByCollectionList: 'nftListByCollectionIdList',
+  nftByIssuer: 'nftListByIssuer',
+  nftByCid: 'nftListByMetadataIdMatch',
+  nftByOwner: 'nftListByOwner',
+  nftCollectedBy: 'nftListCollectedBy',
+  nftSoldBy: 'nftListSoldBy'
 }
 
 export const parsePath = (pathname: string): string[] => {
@@ -23,6 +27,7 @@ export const parsePath = (pathname: string): string[] => {
 }
 
 const hasCall = (call: string | undefined): boolean => call in pathMap
+const getCall = (call: string): ClientCall => pathMap[call]
 const supportChain = (chain: string | undefined): boolean => {
   const uri = getUrl((chain as Prefix) || '')
   return !!uri
@@ -31,7 +36,8 @@ const supportChain = (chain: string | undefined): boolean => {
 const urlOf = (path: string): $URL => new $URL(path)
 const makeQuery = (call: string, id: string): GraphQuery => {
   const client = getClient()
-  const fn: ((id: string) => GraphQuery) | undefined = client[call]
+  const method = getCall(call)
+  const fn: any | undefined = client[method]
   if (fn === undefined || !(typeof fn === 'function')) {
     throw new ReferenceError(`[UNIQUERY::REST] Unable to make call: ${call}`)
   }
