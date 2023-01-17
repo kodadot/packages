693febf70d1c55a1910ba6c1faa8ec390be728fa
Viki Val <viktorko99@gmail.com>
2023-01-17 18:55:07 +0100

    :coffin: kill default import

--- minimark/src/rmrk/index.ts
+++ minimark/src/rmrk/index.ts
@@ -1,1 +1,5 @@
-export * from './unwrap'
+export const unwrap = (rmrk: string): string => {
+  // eslint-disable-next-line no-console
+  console.warn(`Your ${rmrk} is nice but import specific version instead of this one`)
+  throw new Error('Use V1 or V2 instead')
+}
