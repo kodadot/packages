762f4b9da3e730f52a3d48a37cf5e95e166b9866
Viki Val <viktorko99@gmail.com>
2023-02-19 21:32:27 +0100

    :zap: obtain should log error

--- minipfs/src/obtain.ts
+++ minipfs/src/obtain.ts
@@ -3,17 +3,19 @@ import { $fetch } from 'ohmyfetch'
 import { URI } from './types'
 
 export function obtain<T>(uri: URI): Promise<T> {
-  return $fetch<T>(uri)
+  return $fetch<T>(uri, {
+    onRequestError({ error }) {
+      const message = `[KODADOT::MINIPFS] Fail to Obtain: ${error.message}`
+      console.warn(message)
+    }
+  })
 }
 
 export async function obtainSafe<T>(uri: URI): Promise<T> {
   try {
     return await obtain<T>(uri)
   } catch (err) {
-    const message = `[KODADOT::MINIPFS] Fail to Obtain: ${err.message}`
-    console.warn(message)
-    return Promise.reject<T>(message)
-    // throw new Error(`[KODADOT::MINIPFS] Fail to Obtain: ${err.message}`)
+    return {} as T
   }
 }
 
