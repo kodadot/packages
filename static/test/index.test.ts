c59013841c71f608b1a430fb59a6c80567b6f281
Viki Val <viktorko99@gmail.com>
2023-03-03 21:10:50 +0100

    :test_tube: some test

--- static/test/index.test.ts
+++ static/test/index.test.ts
@@ -1,8 +1,12 @@
-import { expect, it, describe } from "vitest";
-import {} from "../src";
+import { expect, it, describe } from 'vitest'
+import { INDEXERS, APOLLO_ENDPOINTS } from '../src'
 
-describe("packageName", () => {
-  it.todo("pass", () => {
-    expect(true).toBe(true);
-  });
-});
+describe('Endpoints', () => {
+  for (const key of Object.keys(INDEXERS)) {
+    it(`should parse SQUID::${key} to APOLLO_INDEXER correctly`, () => {
+      expect(APOLLO_ENDPOINTS[key]).toStrictEqual({
+        httpEndpoint: INDEXERS[key],
+      })
+    })
+  }
+})
