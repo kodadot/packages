92728b06c7da58855f7bf0152b56251cfd124a9d
Matej Holicky <10matejholicky@gmail.com>
2023-04-14 16:02:04 +0200

    style: Fix formatting

--- minimark/src/rmrk/v2/create.ts
+++ minimark/src/rmrk/v2/create.ts
@@ -1,13 +1,13 @@
-import { UpdateFunction } from "../../common/types";
-import { isEmptyString, wrapToString, wrapURI } from "../../utils";
-import { SQUARE } from "../shared/constants";
-import { makeSymbol } from "../shared/identification";
-import { makeCollection } from "../shared/make";
-import { RemarkableString } from "../shared/types";
-import { checkBase } from "./consolidator";
-import { Interaction } from "./enums";
-import { makeRoyalty } from "./helper";
-import { makeBaseSymbol, toSerialNumber } from "./identification";
+import { UpdateFunction } from '../../common/types'
+import { isEmptyString, wrapToString, wrapURI } from '../../utils'
+import { SQUARE } from '../shared/constants'
+import { makeSymbol } from '../shared/identification'
+import { makeCollection } from '../shared/make'
+import { RemarkableString } from '../shared/types'
+import { checkBase } from './consolidator'
+import { Interaction } from './enums'
+import { makeRoyalty } from './helper'
+import { makeBaseSymbol, toSerialNumber } from './identification'
 import {
   CreatedBase,
   CreatedCollection,
@@ -16,71 +16,56 @@ import {
   IRoyaltyAttribute,
   Resource,
   RoyaltyInfo,
-} from "./types";
+} from './types'
 
-const filterEmpty = (field?: string) => !isEmptyString(field);
+const filterEmpty = (field?: string) => !isEmptyString(field)
 
-export const createInteraction: CreateInteractionFunc = ({
-  action,
-  payload,
-}) => {
+export const createInteraction: CreateInteractionFunc = ({ action, payload }) => {
   const convert = (props: string[]): RemarkableString => {
-    const args: string = props.filter(filterEmpty).join(SQUARE);
-    return `RMRK::${action}::2.0.0::${args}`;
-  };
+    const args: string = props.filter(filterEmpty).join(SQUARE)
+    return `RMRK::${action}::2.0.0::${args}`
+  }
 
   switch (action) {
     case Interaction.ACCEPT:
-      return convert([payload.id, payload.entity_type, payload.entity_id]);
+      return convert([payload.id, payload.entity_type, payload.entity_id])
     case Interaction.BASE:
-      return convert([wrapToString(payload.value)]);
+      return convert([wrapToString(payload.value)])
     case Interaction.BUY:
-      return convert([payload.id, payload.recipient ?? ""]);
+      return convert([payload.id, payload.recipient ?? ''])
     case Interaction.CHANGEISSUER:
-      return convert([payload.id, payload.value]);
+      return convert([payload.id, payload.value])
     case Interaction.BURN:
-      return convert([payload.id]);
+      return convert([payload.id])
     case Interaction.CREATE:
-      return convert([wrapToString(payload.value)]);
+      return convert([wrapToString(payload.value)])
     case Interaction.EMOTE:
-      return convert([payload.namespace, payload.id, payload.value]);
+      return convert([payload.namespace, payload.id, payload.value])
     case Interaction.EQUIP:
-      return convert([payload.id, payload.baseslot]);
+      return convert([payload.id, payload.baseslot])
     case Interaction.EQUIPPABLE:
-      return convert([payload.id, payload.slot, payload.value]);
+      return convert([payload.id, payload.slot, payload.value])
     case Interaction.LIST:
-      return convert([payload.id, payload.price]);
+      return convert([payload.id, payload.price])
     case Interaction.LOCK:
-      return convert([payload.id]);
+      return convert([payload.id])
     case Interaction.MINT:
-      return convert([wrapToString(payload.value), payload.recipient ?? ""]);
+      return convert([wrapToString(payload.value), payload.recipient ?? ''])
     case Interaction.RESADD:
-      return convert([
-        payload.id,
-        wrapToString(payload.value),
-        payload.replace,
-      ]);
+      return convert([payload.id, wrapToString(payload.value), payload.replace])
     case Interaction.SEND:
-      return convert([payload.id, payload.recipient]);
+      return convert([payload.id, payload.recipient])
     case Interaction.SETPROPERTY:
-      return convert([
-        payload.id,
-        wrapURI(payload.name),
-        wrapURI(payload.value),
-      ]);
+      return convert([payload.id, wrapURI(payload.name), wrapURI(payload.value)])
     case Interaction.SETPRIORITY:
       // value should always be ',' separated
-      return convert([payload.id, wrapToString(payload.value)]);
+      return convert([payload.id, wrapToString(payload.value)])
     case Interaction.THEMEADD:
-      return convert([
-        payload.base_id,
-        payload.name,
-        wrapToString(payload.value),
-      ]);
+      return convert([payload.base_id, payload.name, wrapToString(payload.value)])
     default:
-      throw new Error(`Unsupported action: ${action}`);
+      throw new Error(`Unsupported action: ${action}`)
   }
-};
+}
 
 // DEV: not sure if trasferable should be
 export const createNFT = (
@@ -94,9 +79,9 @@ export const createNFT = (
 ): CreatedNFT => {
   // checkProps(props)
   // const { symbol, index, transferable = 1, collectionId, metadata } = props
-  const sn = toSerialNumber(index);
-  const instance = makeSymbol(name);
-  const royaltyInfo: IRoyaltyAttribute | undefined = makeRoyalty(royalty);
+  const sn = toSerialNumber(index)
+  const instance = makeSymbol(name)
+  const royaltyInfo: IRoyaltyAttribute | undefined = makeRoyalty(royalty)
   return {
     name, // KodaFlavour, not required by RMRK v2
     sn,
@@ -106,8 +91,8 @@ export const createNFT = (
     symbol: instance,
     properties: royaltyInfo ? { royaltyInfo } : undefined,
     currentOwner: caller,
-  };
-};
+  }
+}
 
 export const createMultipleItem = (
   max: number,
@@ -132,8 +117,8 @@ export const createMultipleItem = (
         royalty,
         caller
       )
-    );
-};
+    )
+}
 
 export const createCollection = (
   caller: string,
@@ -142,21 +127,21 @@ export const createCollection = (
   metadata: string,
   max = 0
 ): CreatedCollection => {
-  return makeCollection(caller, symbol, name ?? "", metadata, max);
-};
+  return makeCollection(caller, symbol, name ?? '', metadata, max)
+}
 
 export const createBase = (props: CreatedBase): CreatedBase => {
-  const { symbol: providedSymbol, parts = [], themes } = props;
-  const symbol = makeBaseSymbol(providedSymbol);
-  checkBase({ symbol, parts, themes });
+  const { symbol: providedSymbol, parts = [], themes } = props
+  const symbol = makeBaseSymbol(providedSymbol)
+  checkBase({ symbol, parts, themes })
   return {
     ...props,
     symbol,
-  };
-};
+  }
+}
 
 export const createResource = (_obj: any): Resource | null => {
   // eslint-disable-next-line no-console
-  console.warn("createResource is not implemented yet");
-  return null;
-};
+  console.warn('createResource is not implemented yet')
+  return null
+}
--- minimark/src/rmrk/v2/types.ts
+++ minimark/src/rmrk/v2/types.ts
@@ -3,31 +3,27 @@ import {
   AbstractCreatedCollection,
   AbstractCreatedNFT,
   UnwrappedRemark as AbstractRemarkWrapper,
-} from "../shared/types";
-import { Interaction } from "./enums";
+} from '../shared/types'
+import { Interaction } from './enums'
 
-export type OnlyMintInteraction = Interaction.CREATE | Interaction.MINT;
-export type JustInteraction = Exclude<
-  Interaction,
-  OnlyMintInteraction | Interaction.UNLIST
->;
+export type OnlyMintInteraction = Interaction.CREATE | Interaction.MINT
+export type JustInteraction = Exclude<Interaction, OnlyMintInteraction | Interaction.UNLIST>
 
-export type InteractionUnion = keyof typeof Interaction;
-// type AvailableAction = Exclude<InteractionUnion, Interaction.UNLIST>
+export type InteractionUnion = keyof typeof Interaction
 
-export type UnwrappedRemark<T> = AbstractRemarkWrapper<T, Interaction>;
+export type UnwrappedRemark<T> = AbstractRemarkWrapper<T, Interaction>
 
 export type CreatedCollection = AbstractCreatedCollection & {
-  properties?: IProperties;
-};
+  properties?: IProperties
+}
 
 export type Base = {
-  value: CreatedBase;
-};
+  value: CreatedBase
+}
 
 export type WithId = {
-  id: string;
-};
+  id: string
+}
 
 // more fancy
 // export type WithValue<T extends boolean = true> = {
@@ -35,81 +31,81 @@ export type WithId = {
 // }
 
 export type WithValue = {
-  value: string;
-};
+  value: string
+}
 
-export type BasicInteraction = WithId & WithValue;
+export type BasicInteraction = WithId & WithValue
 
 export type Accept = {
-  id: string;
-  entity_type: "RES" | "NFT";
-  entity_id: string;
-};
+  id: string
+  entity_type: 'RES' | 'NFT'
+  entity_id: string
+}
 
 export type Equip = WithId & {
-  baseslot: string;
-};
+  baseslot: string
+}
 
 export type Equippable = BasicInteraction & {
-  slot: string;
-};
+  slot: string
+}
 
-export type Lock = WithId;
+export type Lock = WithId
 
 export type Resadd = WithId & {
-  value: Resource;
-  replace: string;
-};
+  value: Resource
+  replace: string
+}
 
 export type SetPriority = WithId & {
-  value: string[];
-};
+  value: string[]
+}
 
 export type SetProperty = BasicInteraction & {
-  name: string;
-};
+  name: string
+}
 
 export type ThemeAdd = {
-  base_id: string;
-  name: string;
-  value: Record<string, string>;
-};
+  base_id: string
+  name: string
+  value: Record<string, string>
+}
 
 export type Buy = {
-  id: string;
-  recipient?: string;
-};
+  id: string
+  recipient?: string
+}
 
 export type Emote = {
-  id: string;
-  value: string;
-  namespace: string;
-};
+  id: string
+  value: string
+  namespace: string
+}
 
 export type Send = WithId & {
-  recipient: string;
-};
+  recipient: string
+}
 
 export type List = {
-  id: string;
-  price: string;
-};
+  id: string
+  price: string
+}
 
 export type ChangeIssuer = {
-  id: string;
-  value: string;
-};
+  id: string
+  value: string
+}
 
 export type Create = {
-  value: CreatedCollection;
-};
+  value: CreatedCollection
+}
 
 export type Mint = {
-  value: CreatedNFT;
-  recipient?: string;
-};
+  value: CreatedNFT
+  recipient?: string
+}
 
-export type Burn = WithId;
+export type Burn = WithId
 
 export type InteractionValue =
   | Accept
@@ -129,99 +125,91 @@ export type InteractionValue =
   | ChangeIssuer
   | Create
   | Mint
-  | Burn;
+  | Burn
 
 // from https://github.com/rmrk-team/rmrk-spec/blob/master/standards/rmrk2.0.0/entities/collection.md#properties-format
-export type IProperties = Record<string, IAttribute | IRoyaltyAttribute>;
+export type IProperties = Record<string, IAttribute | IRoyaltyAttribute>
 
 export interface IAttribute {
   _mutation?: {
-    allowed: boolean;
+    allowed: boolean
     with?: {
-      opType: Interaction;
-      condition?: string;
-    };
-  };
-  type:
-    | "array"
-    | "object"
-    | "int"
-    | "float"
-    | "boolean"
-    | "datetime"
-    | "string"
-    | "royalty";
-  value: any;
+      opType: Interaction
+      condition?: string
+    }
+  }
+  type: 'array' | 'object' | 'int' | 'float' | 'boolean' | 'datetime' | 'string' | 'royalty'
+  value: any
 }
 
 export interface IRoyaltyAttribute extends IAttribute {
-  type: "royalty";
+  type: 'royalty'
   value: {
-    receiver: string;
-    royaltyPercentFloat: number;
-  };
+    receiver: string
+    royaltyPercentFloat: number
+  }
 }
 
 export type CreatedNFT = AbstractCreatedNFT & {
-  name?: string;
-  symbol: string;
-  properties?: IProperties;
-  currentOwner?: string;
-};
-
-type Theme = string | Record<string, string>;
-
-type Themes = Record<string, Theme>;
-
-export interface BasicPart {
-  id: string;
-  metadata: string;
+  name?: string
+  symbol: string
+  properties?: IProperties
+  currentOwner?: string
 }
 
-export type PartType = "slot" | "fixed";
+type Theme = string | Record<string, string>
+
+type Themes = Record<string, Theme>
+
+export interface BasicPart {
+  id: string
+  metadata: string
+}
+
+export type PartType = 'slot' | 'fixed'
 
 // https://github.com/rmrk-team/rmrk-tools/blob/b3fe70f59db383d3b4d158bb0b4c3c88aaaf6158/src/rmrk2.0.0/classes/base.ts#L170
 export interface AbstractBasePart {
-  id: string;
-  src: string;
-  type: PartType;
-  z?: number;
-  thumb?: string;
-  metadata?: string;
+  id: string
+  src: string
+  type: PartType
+  z?: number
+  thumb?: string
+  metadata?: string
 }
 
 export interface FixedPart extends AbstractBasePart {
-  type: "fixed";
+  type: 'fixed'
 }
 
 export interface SlotPart extends AbstractBasePart {
-  type: "slot";
-  equippable: string[] | "*";
+  type: 'slot'
+  equippable: string[] | '*'
 }
 
-type BasePart = FixedPart | SlotPart;
+type BasePart = FixedPart | SlotPart
 
 export type Resource = {
-  id: string; // 'nanoid-of-resource',
-  base?: string; // 'base-uri',
-  src?: string; // 'media-uri', // points to image or video
-  metadata?: string; // 'metadata-uri',
-  slot?: string; // 'baseslot',
-  thumb?: string; // 'uri-of-thumbnail',
-};
+  id: string // 'nanoid-of-resource',
+  base?: string // 'base-uri',
+  src?: string // 'media-uri', // points to image or video
+  metadata?: string // 'metadata-uri',
+  slot?: string // 'baseslot',
+  thumb?: string // 'uri-of-thumbnail',
+}
 
-export type CreateInteractionFunc = (props: CreateInteractionProps) => string;
+export type CreateInteractionFunc = (props: CreateInteractionProps) => string
 
 // From RMRK:TOOLS
 // https://github.com/rmrk-team/rmrk-tools/blob/master/src/rmrk2.0.0/tools/types.ts
-export type BaseType = "svg" | "png" | "audio" | "video" | "mixed" | string;
+export type BaseType = 'svg' | 'png' | 'audio' | 'video' | 'mixed' | string
 
 export interface CreatedBase {
-  symbol: string;
-  type?: BaseType;
-  themes?: Themes;
-  parts: BasePart[];
-  metadata?: string;
+  symbol: string
+  type?: BaseType
+  themes?: Themes
+  parts: BasePart[]
+  metadata?: string
 }
 
 // usage // | ActionWithPayload<'CREATE', Create>
@@ -244,46 +232,46 @@ export type CreateInteractionProps =
   | { action: Interaction.CHANGEISSUER; payload: ChangeIssuer }
   | { action: Interaction.CREATE; payload: Create }
   | { action: Interaction.MINT; payload: Mint }
-  | { action: Interaction.BURN; payload: Burn };
+  | { action: Interaction.BURN; payload: Burn }
 
 export type UnwrapValue = {
-  [Interaction.ACCEPT]: Accept;
-  [Interaction.BASE]: Base;
-  [Interaction.EQUIP]: Equip;
-  [Interaction.EQUIPPABLE]: Equippable;
-  [Interaction.LOCK]: Lock;
-  [Interaction.RESADD]: Resadd;
-  [Interaction.SETPRIORITY]: SetPriority;
-  [Interaction.SETPROPERTY]: SetProperty;
-  [Interaction.THEMEADD]: ThemeAdd;
-  [Interaction.BUY]: Buy;
-  [Interaction.EMOTE]: Emote;
-  [Interaction.SEND]: Send;
-  [Interaction.LIST]: List;
-  [Interaction.CHANGEISSUER]: ChangeIssuer;
-  [Interaction.CREATE]: Create;
-  [Interaction.MINT]: Mint;
-  [Interaction.BURN]: Burn;
-  NONE: BasicInteraction;
-};
+  [Interaction.ACCEPT]: Accept
+  [Interaction.BASE]: Base
+  [Interaction.EQUIP]: Equip
+  [Interaction.EQUIPPABLE]: Equippable
+  [Interaction.LOCK]: Lock
+  [Interaction.RESADD]: Resadd
+  [Interaction.SETPRIORITY]: SetPriority
+  [Interaction.SETPROPERTY]: SetProperty
+  [Interaction.THEMEADD]: ThemeAdd
+  [Interaction.BUY]: Buy
+  [Interaction.EMOTE]: Emote
+  [Interaction.SEND]: Send
+  [Interaction.LIST]: List
+  [Interaction.CHANGEISSUER]: ChangeIssuer
+  [Interaction.CREATE]: Create
+  [Interaction.MINT]: Mint
+  [Interaction.BURN]: Burn
+  NONE: BasicInteraction
+}
 
 export type CreateNFTProps = {
-  collectionId: string;
-  symbol: string;
-  transferable: number;
-  index: number;
-  metadata: string;
-};
+  collectionId: string
+  symbol: string
+  transferable: number
+  index: number
+  metadata: string
+}
 
 export type RoyaltyInfo = {
-  receiver: string;
-  percent: number;
-};
+  receiver: string
+  percent: number
+}
 
 export type EquippableOption = {
-  operation: "+" | "-" | "*";
-  collection: string;
-};
+  operation: '+' | '-' | '*'
+  collection: string
+}
 
 // function fn<T extends keyof UnwrapValue = 'NONE'>(value: Record<any, any>): UnwrapValue[T] {
 //   return value as UnwrapValue[T]
