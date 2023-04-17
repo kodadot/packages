64a6e526ef46331c407964a4edc8f0a032694a12
Viki Val <viktorko99@gmail.com>
2023-04-17 15:50:14 +0200

    :test_tube: properties

--- minimark/test/v2/remarkable/properties.test.ts
+++ minimark/test/v2/remarkable/properties.test.ts
@@ -1,7 +1,11 @@
-
 import { describe, expect, it } from 'vitest'
-import { resolveRoyalty } from '../../../src/rmrk/v2/helper'
+import {
+  convertAttributesToProperties,
+  mergeProperties,
+  resolveRoyalty
+} from '../../../src/rmrk/v2/helper'
 import { IProperties } from '../../../src/rmrk/v2/types'
+import { Attribute } from '../../../src/common'
 
 describe('RMRK2 Properties', () => {
   it('royaltyinfo should be extracted', () => {
@@ -19,6 +23,62 @@ describe('RMRK2 Properties', () => {
     expect(royalty).toEqual({
       receiver: 'GksmaSfxzmD2Bxh7MZBTyGXTnGrQwoKnvukekh7JVzD8RVC',
       percent: 20
+    })
+  })
+
+  it('should be converted from attributes', () => {
+    const attribute: Attribute[] = [
+      {
+        trait_type: 'dress',
+        value: 'black'
+      },
+      {
+        trait_type: 'hair',
+        value: 'pink'
+      }
+    ]
+
+    const properties = convertAttributesToProperties(attribute)
+    expect(properties).toEqual({
+      dress: {
+        type: 'string',
+        value: 'black'
+      },
+      hair: {
+        type: 'string',
+        value: 'pink'
+      }
+    })
+  })
+
+  it('should be merged', () => {
+    const defaultProps: IProperties = {
+      dress: {
+        type: 'string',
+        value: 'black'
+      }
+    }
+
+    const properties = mergeProperties(defaultProps, 'royaltyInfo', {
+      type: 'royalty',
+      value: {
+        receiver: 'GksmaSfxzmD2Bxh7MZBTyGXTnGrQwoKnvukekh7JVzD8RVC',
+        royaltyPercentFloat: 20
+      }
+    })
+
+    expect(properties).toEqual({
+      dress: {
+        type: 'string',
+        value: 'black'
+      },
+      royaltyInfo: {
+        type: 'royalty',
+        value: {
+          receiver: 'GksmaSfxzmD2Bxh7MZBTyGXTnGrQwoKnvukekh7JVzD8RVC',
+          royaltyPercentFloat: 20
+        }
+      }
     })
   })
 })
