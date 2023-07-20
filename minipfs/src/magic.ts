03928165a86aba190bbfcb9130e11ba56594edca
Viki Val <viktorko99@gmail.com>
2023-07-20 16:03:35 +0200

    :zap: apply formatter when needed

--- minipfs/src/magic.ts
+++ minipfs/src/magic.ts
@@ -5,8 +5,12 @@ import { sanitize } from './sanitize'
 import { HTTPS_URI, IPFS_PATH, IPNS_PATH, SanitizedOutput, URI } from './types'
 
 export function $obtain<T>(uri: URI | string, providers: AvailableProviders = [], fast?: boolean): Promise<T> {
-  const { needProvider, path }: SanitizedOutput = sanitize(uri)
+  const { needProvider, path, formatter }: SanitizedOutput = sanitize(uri)
   const callback = fast ? obtainFast : obtain
+
+  if (formatter) {
+    return Promise.resolve(formatter<T>(path as string))
+  }
 
   if (needProvider) {
     return competition<T>(path as IPFS_PATH | IPNS_PATH, providers, callback)
