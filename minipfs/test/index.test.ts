a9b1f56dc0119c8c1c39257174ef35017606bb16
Viki Val <viktorko99@gmail.com>
2023-09-08 14:02:04 +0200

    :test_tube: native urls

--- minipfs/test/index.test.ts
+++ minipfs/test/index.test.ts
@@ -4,7 +4,8 @@ import { isFullPath } from '../src'
 describe('isFullPath', () => {
   const tests = [
     { input: 'ipfs://ipfs/bafybeihrnpphrgs5os47w35a5ypqt3mqashd5tn3v27pqt2iiw3hikavhm', output: true },
-    { input: 'ipfs://bafybeihrnpphrgs5os47w35a5ypqt3mqashd5tn3v27pqt2iiw3hikavhm', output: true }
+    { input: 'ipfs://bafybeihrnpphrgs5os47w35a5ypqt3mqashd5tn3v27pqt2iiw3hikavhm', output: true },
+    { input: 'ipfs://bafybeibe6ihmid6i5a777n4znvnovok6szkju6ijde4wzprd2zkntqj4gi/4.json', output: true }
   ]
 
   for (const test of tests) {
