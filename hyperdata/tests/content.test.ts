a803a3f86fd0b87ee813cb989251d0a15b26b1d7
Viki Val <viktorko99@gmail.com>
2023-10-19 13:33:00 +0200

    :test_tube: Valid parse of Koda Metadata

--- hyperdata/tests/content.test.ts
+++ hyperdata/tests/content.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest'
-import { TezosMetadata, contentFrom } from '../src'
+import { OpenSeaMetadata, TezosMetadata, contentFrom } from '../src'
 import fxhash from './examples/fxhash.json'
+import koda from './examples/koda.json'
 
 describe('contentFrom', () => {
   it(`should parse FXhash metadata to Content correctly`, () => {
@@ -16,6 +17,39 @@ describe('contentFrom', () => {
       generative: undefined,
       tags: [],
       thumbnail: metadata.thumbnailUri,
+      type: '',
+    })
+  })
+
+  it(`should parse Koda metadata to Content correctly`, () => {
+    const metadata = koda as OpenSeaMetadata
+    expect(contentFrom(metadata)).toStrictEqual({
+      _raw: undefined,
+      name: metadata.name,
+      description: metadata.description,
+      image: metadata.image,
+      animationUrl: metadata.animation_url,
+      attributes: [
+        {
+          display: '',
+          trait: 'ID',
+          value: '1082',
+        },
+        {
+          display: '',
+          trait: 'Type',
+          value: 'Dolphin',
+        },
+        {
+          display: '',
+          trait: 'Rarity',
+          value: 'Rare',
+        },
+      ],
+      externalUrl: metadata.external_url,
+      generative: undefined,
+      tags: [],
+      thumbnail: undefined,
       type: '',
     })
   })
