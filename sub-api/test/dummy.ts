f19877a2018ff24d6d99819cd05831718ea891e2
Viki Val <viktorko99@gmail.com>
2024-09-20 16:01:21 +0200

    :test_tube: new KSM RPC

--- sub-api/test/dummy.ts
+++ sub-api/test/dummy.ts
@@ -1,6 +1,6 @@
 export enum Endpoint {
   MOVR = 'wss://moonriver.public.blastapi.io',
   AHP = 'wss://polkadot-asset-hub-rpc.polkadot.io',
-  KSM = 'wss://kusama.api.onfinality.io/public-ws',
+  KSM = 'wss://rpc.ibp.network/kusama',
   LOCAL = 'ws://127.0.0.1:9944',
 }
